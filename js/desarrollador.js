1f4610862e74275ca15547a674d44042259ed7cd
Sebastián M Canosa <33286208+scanosa@users.noreply.github.com>
2023-12-16 12:12:10 -0300

    mejoras v1.1
    Nuevo CSS, Catalogo y pass de acceso

--- js/desarrollador.js
+++ js/desarrollador.js
@@ -1,3 +1,14 @@
 function autor() {
-    alert("Autora: Emilia Hassan. E-mail: emiliahassan@hotmail.com");
+    var curso1 = "Fullstack Python \n";
+    var programa1 = "<Codo a Codo/> | 2023\n";
+    var prof1 = "Prof. Nicole Schwab\n";
+    var autor1 = "Emilia Hassan - emiliahassan@hotmail.com\n";
+    var autor2 = "Federico Farola - farolafede2@gmail.com\n";
+    var autor3 = "Gustavo Recalde - recgus@gmail.com\n";
+    var autor4 = "Sebastián Canosa - scanosa@proton.me\n";
+
+    var mensaje = curso1 + programa1 + prof1 + "Desarrolladores:\n" + autor1 +  autor2 + autor3 + autor4;
+
+    alert(mensaje);
+    
 }
